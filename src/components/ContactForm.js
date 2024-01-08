91eef5411166c6587e26a8a2c10a81bdf17f560a
GHAZALI LOUBNA <loubna.ghazali16@gmail.com>
2024-01-08 11:22:25 +0100

    don't know

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import emailjs from 'emailjs-com';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function ContactForm() {
-  // Send via email using emailjs
-  // prevent form default action
-  var sendEmail = (e) => {
+  const initialFormData = { id: '', name: '', email: '', message: '' };
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(process.env.REACT_APP_service, process.env.REACT_APP_template, e.target, process.env.REACT_APP_user)
-      .then((result) => {
-        alert("Message sent\n Status: " + result.text);
-      }, (error) => {
-        alert("Message not sent\n Status: " + error.text);
+    axios.post('http://localhost:8085/contacts', formData)
+      .then((response) => {
+        alert("Message sent\nStatus: " + response.statusText);
+        setFormData(initialFormData); // Reset the form fields to initial values
+      })
+      .catch((error) => {
+        alert("Message not sent\nStatus: " + error.response.statusText);
       });
-
-    e.target.reset();
-  }
+  };
 
   return (
     <div className="card text-center mt-4 crv-8 p-3 bx-shadow" style={{ backgroundColor: '#b5777e' }}>
       <h4 className="text-left font-weight-bold">
         Let's talk
       </h4>
-      <form onSubmit={sendEmail} method="post" className="mt-2">
+      <form onSubmit={handleSubmit} method="post" className="mt-2">
         <div className="input-group">
           <div className="input-group-prepend">
             <span className="input-group-text ion-person" style={{ marginRight: '15px' }}></span>
           </div>
-          <input type="text" className="form-control" id="name" name="name" placeholder="Name (Optional)" />
+          <input type="text" className="form-control" id="name" name="name" onChange={handleChange} value={formData.name} placeholder="Name (Optional)" />
         </div>
         <div className="input-group mt-2">
           <div className="input-group-prepend">
             <span className="input-group-text ion-email" style={{ marginRight: '5px' }}></span>
           </div>
-          <input type="text" className="form-control" id="email" name="email" required placeholder="Email" />
+          <input type="text" className="form-control" id="email" name="email" onChange={handleChange} value={formData.email} required placeholder="Email" />
         </div>
-        <textarea name="message" id="message" rows="5" className="form-control mt-2" placeholder="Message"></textarea>
-        <button className="btn custom-btn-color btn-block mt-3 crv-25" style={{ backgroundColor: '#77b5ae' }}>Send now</button>
+        <textarea name="message" id="message" rows="5" className="form-control mt-2" onChange={handleChange} value={formData.message} placeholder="Message"></textarea>
+        <button type="submit" className="btn custom-btn-color btn-block mt-3 crv-25" style={{ backgroundColor: '#77b5ae' }}>Send now</button>
       </form>
     </div>
   );
